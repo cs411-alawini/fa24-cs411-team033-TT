68a9a7fcb41db7df97b33a8b6cf596aa879a21a6
ytcheng0214 <ytcheng4@illinois.edu>
2024-12-02 18:29:13 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,57 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Closet from './Closet'; // Import your Closet component
+// import Calendar from './Calendar'; // Import your Calendar component
+// import NavBar from './components/NavBar';
+// import Login from './Login';
+
+// const App = () => {
+//   return (
+//       <Router>
+//         <NavBar/>
+//         <Routes>
+//           <Route path="/" element={<Login />} /> {/* Default route */}
+//           <Route path="/closet" element={<Closet />} />
+//           <Route path="/calendar" element={<Calendar />} />
+//           <Route path="/login" element={<Login />} />
+//         </Routes>
+//       </Router>
+//   );
+// };
+
+// export default App;
+
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Closet from './Closet'; // Import your Closet component
 import Calendar from './Calendar'; // Import your Calendar component
 import NavBar from './components/NavBar';
+import Login from './Login';
 
 const App = () => {
+  const location = useLocation(); // Get the current location
+  const pathsWithoutNavBar = ['/', '/login']; // Define routes without NavBar
+
   return (
-      <Router>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<Closet />} /> {/* Default route */}
-          <Route path="/closet" element={<Closet />} />
-          <Route path="/calendar" element={<Calendar />} />
-        </Routes>
-      </Router>
+    <>
+      {/* Render NavBar only if the current path is not in pathsWithoutNavBar */}
+      {!pathsWithoutNavBar.includes(location.pathname) && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Login />} /> {/* Default route */}
+        <Route path="/closet" element={<Closet />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 };
 
-export default App;
+// Wrap App with Router
+const AppWithRouter = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWithRouter;
